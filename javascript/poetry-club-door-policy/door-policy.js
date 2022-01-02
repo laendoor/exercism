44e882da6bfe111768f0fa9778f28965baa1c5f6
Leandro Di Lorenzo <3334920+leandrojdl@users.noreply.github.com>
2022-01-02 09:16:54 -0300

    js: poetry club door policy

---
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+const capitalize = word => word.toLowerCase().replace(/^\w/, l => l.toUpperCase())
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export const frontDoorResponse = line => line.charAt(0); 
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export const frontDoorPassword = word => capitalize(word);
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export const backDoorResponse = line => line.charAt(line.trim().length - 1);
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ */
+export const backDoorPassword = word => `${capitalize(word)}, please`;
