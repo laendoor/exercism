693a0efd4732332b80972386f8d4a73f955de4af
Leandro Di Lorenzo <3334920+leandrojdl@users.noreply.github.com>
2022-01-02 09:16:00 -0300

    js: freelancer rates

---
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export const dayRate = ratePerHour => 8 * ratePerHour;
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export const daysInBudget = (budget, ratePerHour) => Math.floor(budget / ratePerHour / 8);
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const nonMonthDays = numDays % 22;
+  const totalDays = ((numDays - nonMonthDays) * (1 - discount)) + nonMonthDays;
+  return Math.ceil(totalDays * dayRate(ratePerHour));
+}
